ab039ed911c59bc4e491e59b9fff5fc197459ca1
PTC <thqv74@gmail.com>
2021-03-23 16:52:49 -0300

    First commit

--- Form1.cs
+++ Form1.cs
@@ -5,13 +5,6 @@
 //**********************************************************************************************************//
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SOS_Battery_Plant
--- Form2.cs
+++ Form2.cs
@@ -5,13 +5,7 @@
 //**********************************************************************************************************//
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb; // Database
 
